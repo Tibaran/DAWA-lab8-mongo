68d214e9269b9c26edd878ef620599cb1b532672
Tibaran <palacios.tibaran@gmail.com>
2021-10-14 02:55:06 -0500

    Laboratorio7 - Mongo

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {User} = require('../schemas/userModel');
+const {Rol} = require('../schemas/rolModel');
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
-    console.log('Time: ', Date.now());
+    //console.log('Time: ', Date.now());
     next();
   });
-router.get('/', (req, res)=>{
-    res.send("hello1");
+router.get('/', async(req, res)=>{
+    const Users = await User.find();
+    res.render('layout', {vista: 'users', user: Users});  
 });
-router.get('/:id', (req, res)=>{
-    res.send("hello1");
+router.get('/add', async(req, res)=>{
+    const Rols = await Rol.find();
+    res.render('layout', {vista: 'usersAdd', rol: Rols});
 });
-router.post('/', (req, res)=>{
-    res.send("hello1");
+router.post('/add', async(req, res)=>{
+    const {txtNombre, txtCorreo, txtPass, txtImg, txtRol, txtEstado}= req.body;
+    const id = await User.estimatedDocumentCount()+1;
+    const newUser = new User({_id: id, rol: txtRol, nombre: txtNombre, correo: txtCorreo, password: txtPass, img: txtImg, estado: txtEstado=='on'?true:false});
+    await newUser.save();
+    res.redirect('/users');
 });
 
 module.exports = router;
--- schemas/userModel.js
+++ schemas/userModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const { rolSchema } = require('./rolModel');
+const { rolSchema, Rol } = require('./rolModel');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
     _id: {type: String, require: true},
-    rol: {type: rolSchema, require: true},
+    rol: {type: String, require: true},
     nombre: {type: String, required: true},
     correo: {type: String, required: true},
     password: {type: String, required: true},
